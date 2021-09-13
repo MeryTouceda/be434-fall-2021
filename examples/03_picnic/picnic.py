0b0debdfce872068731b6ad859328f0bf6f34aec
Mery <maria.touceda.suarez@gmail.com>
2021-09-13 10:43:59 -0700

    finished picnic.py
    picnic.py has been created and completed

--- examples/03_picnic/picnic.py
+++ examples/03_picnic/picnic.py
@@ -13,38 +13,19 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
+        description='Picnic game',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
+    parser.add_argument('item',
                         metavar='str',
-                        help='A positional argument')
+                        nargs='+',
+                        help='Item(s) to bring')
 
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+                        
+    parser.add_argument('-s', '--sorted',
+                        help='Sort the items',
+                        action = 'store_true') 
+              
 
     return parser.parse_args()
 
@@ -54,17 +35,24 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
+    items = args.item   
+    
+    if args.sorted: 
+        items.sort()                                     
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    bringing = '' #initialize an empty string to store the modified list
+    if len(items) == 1: 
+        bringing = items[0]
+    elif len(items) == 2:
+        bringing = ' and '.join(items)
+    else: 
+        items[-1] = 'and ' + items[-1] 
+        bringing = ', '.join(items) 
+        
+
+    print(" You are bringing {}.".format(bringing))
+
+   
 
 
 # --------------------------------------------------
