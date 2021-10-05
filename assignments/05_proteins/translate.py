8b8a81e97106331f67509bc72e5afb477142a158
Mery <maria.touceda.suarez@gmail.com>
2021-10-05 08:30:00 -0700

    it works but no missing codons
    the code works, but it doesn't pass the missing codons test

--- assignments/05_proteins/translate.py
+++ assignments/05_proteins/translate.py
@@ -46,28 +46,27 @@ def main():
     args = get_args()
     seq = args.sequence
     fh = args.codons # I don't need to do open because I don't have a string, I have a file (handle?) directly
-    #out_fh = args.outfile
+    out_fh = args.outfile
     
     # read codons into a dictionary
     codons = dict()
     for line in fh: 
         key, value = line.split()
-        #key, value = line.rstrip().split() ## TRY THIS OPTION AND SEE IF IT WORKS
+        #key, value = line.rstrip().split() # this seems to do the same
         codons[key] = value
-    pprint(codons) # to check codons is created right
+    #pprint(codons) # to check codons is created right
 
     # translate the sequence
-    k = 3
-    for kmer in [seq[i:i + k] for i in range(0, len(seq) - k + 1)]:
+    k = 3 #set to desired kmer length
+    proteinseq = list()
+    for kmer in [seq[i:i + k] for i in range(0, len(seq) - k + 1)]: #loop over kmers in input sequence (sliding window = 1)
         #print(kmer) # to check for loop works
-        if kmer.upper() in codons:  ### THERE IS A PROBLEM HERE!!
-            print(codons.get(kmer,'-'))
-        
-        # if kmer in codons: 
-        #     out_fh = open(args.output, 'wt')
-        #     out_fh.write(codons.get(kmer,'-'))                              
-        #     out_fh.close()
-        #     print("Output written to {}".format(out_fh.name))
+        #if kmer.upper() in codons: #i don't think i need this
+        proteinseq.append(codons.get(kmer.upper(),'-'))
+    #out_fh = open(args.outfile, 'wt')
+    out_fh.write(''.join(proteinseq))                              
+    out_fh.close()
+    print("Output written to {}".format(out_fh.name))
 
 # --------------------------------------------------
 if __name__ == '__main__':
