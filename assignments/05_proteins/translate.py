dcc46913212c057bcfaf1e5538a3f654422a8da4
Mery <maria.touceda.suarez@gmail.com>
2021-10-05 08:48:29 -0700

    all tests but format
    All tests passed except from the formatting ones

--- assignments/05_proteins/translate.py
+++ assignments/05_proteins/translate.py
@@ -27,6 +27,7 @@ def get_args():
                         '--codons',
                         help='A file with codon translations',
                         metavar='FILE',
+                        required= True, 
                         type=argparse.FileType('rt'))
 
     parser.add_argument('-o',
@@ -45,28 +46,28 @@ def main():
 
     args = get_args()
     seq = args.sequence
-    fh = args.codons # I don't need to do open because I don't have a string, I have a file (handle?) directly
+    fh = args.codons
     out_fh = args.outfile
     
     # read codons into a dictionary
     codons = dict()
     for line in fh: 
         key, value = line.split()
-        #key, value = line.rstrip().split() # this seems to do the same
+        #key, value = line.rstrip().split()                         # this seems to do the same
         codons[key] = value
     #pprint(codons) # to check codons is created right
 
     # translate the sequence
-    k = 3 #set to desired kmer length
-    proteinseq = list()
-    for kmer in [seq[i:i + k] for i in range(0, len(seq) - k + 1)]: #loop over kmers in input sequence (sliding window = 1)
-        #print(kmer) # to check for loop works
-        #if kmer.upper() in codons: #i don't think i need this
-        proteinseq.append(codons.get(kmer.upper(),'-'))
-    #out_fh = open(args.outfile, 'wt')
-    out_fh.write(''.join(proteinseq))                              
+    k = 3                                                           # set to desired kmer length
+    proteinseq = list()                                             # initialize sequence to store the protein sequence
+    for kmer in [seq[i:i + k] for i in range(0, len(seq), k)]:      # loop over kmers in sequence (sliding window = k)
+        #print(kmer)                                                # to check for loop works
+        proteinseq.append(codons.get(kmer.upper(),'-'))             # add value (AA) of kmer key (codon) to list
+
+    # write output to file 
+    out_fh.write(''.join(proteinseq))                              # write the contents of list as a string to file
     out_fh.close()
-    print("Output written to {}".format(out_fh.name))
+    print('Output written to "{}".'.format(out_fh.name))
 
 # --------------------------------------------------
 if __name__ == '__main__':
