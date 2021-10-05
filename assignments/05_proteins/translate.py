85b69910a0bb797be351164f42527e333f954c97
Mery <maria.touceda.suarez@gmail.com>
2021-10-04 21:09:59 -0700

    some improvements

--- assignments/05_proteins/translate.py
+++ assignments/05_proteins/translate.py
@@ -8,6 +8,7 @@ Purpose: Translate DNA/RNA to proteins
 import argparse
 import os
 #import sys
+from pprint import pprint
 
 
 # --------------------------------------------------
@@ -29,7 +30,7 @@ def get_args():
                         type=argparse.FileType('rt'))
 
     parser.add_argument('-o',
-                        '--output',
+                        '--outfile',
                         help='Output filename',
                         metavar='FILE',
                         type=argparse.FileType('wt'),
@@ -45,25 +46,28 @@ def main():
     args = get_args()
     seq = args.sequence
     fh = args.codons # I don't need to do open because I don't have a string, I have a file (handle?) directly
-    out_fh = args.output
+    #out_fh = args.outfile
     
     # read codons into a dictionary
     codons = dict()
     for line in fh: 
         key, value = line.split()
+        #key, value = line.rstrip().split() ## TRY THIS OPTION AND SEE IF IT WORKS
         codons[key] = value
+    pprint(codons) # to check codons is created right
 
     # translate the sequence
     k = 3
     for kmer in [seq[i:i + k] for i in range(0, len(seq) - k + 1)]:
-        if kmer in codons: 
-            #out_fh = open(args.output, 'wt')
-            out_fh.write(codons.get(kmer,'-'))                              
-            out_fh.close()
-            print("Output written to {}".format(out_fh.name))
-
-
-
+        #print(kmer) # to check for loop works
+        if kmer.upper() in codons:  ### THERE IS A PROBLEM HERE!!
+            print(codons.get(kmer,'-'))
+        
+        # if kmer in codons: 
+        #     out_fh = open(args.output, 'wt')
+        #     out_fh.write(codons.get(kmer,'-'))                              
+        #     out_fh.close()
+        #     print("Output written to {}".format(out_fh.name))
 
 # --------------------------------------------------
 if __name__ == '__main__':
