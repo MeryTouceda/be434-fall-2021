8e5a53de69681a4e0ea9c18f569d8f550fc59b11
Mery <maria.touceda.suarez@gmail.com>
2021-10-18 11:00:57 -0700

    Completed iupac.py

--- assignments/07_iupac/iupac.py
+++ assignments/07_iupac/iupac.py
@@ -2,7 +2,7 @@
 """
 Author : mtoucedasuarez <mtoucedasuarez@localhost>
 Date   : 2021-10-13
-Purpose: Rock the Casbah
+Purpose: Return IUPAC code from sequence
 """
 
 import argparse
@@ -19,7 +19,7 @@ def get_args():
 
     parser.add_argument('seq',
                         metavar='SEQ',
-                        nargs= '+',
+                        nargs='+',
                         help='Input sequence(s)')
 
     parser.add_argument('-o',
@@ -40,35 +40,32 @@ def main():
     sequence_list = args.seq
 
     # create the dictionary with the code
-    iupac = {'A':'A', 'C':'C', 'G':'G', 'T':'T', 'U':'U', 
-            'R':'AG', 'Y':'CT', 'S':'GC', 'W':'AT', 'K':'GT',
-            'M':'AC', 'B':'CGT', 'D':'AGT', 'H':'ACT', 
-            'V':'ACG', 'N': 'ACGT'}
-    
+    iupac = {'A': 'A', 'C': 'C', 'G': 'G', 'T': 'T', 'U': 'U',
+             'R': 'AG', 'Y': 'CT', 'S': 'GC', 'W': 'AT', 'K': 'GT',
+             'M': 'AC', 'B': 'CGT', 'D': 'AGT', 'H': 'ACT',
+             'V': 'ACG', 'N': 'ACGT'}
+
     # for every sequence in the list of sequences provided
     for seq in sequence_list:
-        output_seq = "" # initialize output sequence
-        output_re = "" # initialize output regular expression
+        output_seq = ""  # initialize output sequence
+        output_re = ""  # initialize output regular expression
 
         # for every item (letter) in the sequence
-        for item in seq: 
+        for item in seq:
             # if the value in dictionary is more than one base
             if len(iupac.get(item)) > 1:
-                #output_seq = output_seq + item
+                output_seq = output_seq + item
                 # store the regular expression with brakets
                 output_re = output_re + '[{}]'.format(iupac.get(item))
-            else: 
+            else:
                 output_seq = output_seq + item
                 output_re = output_re + iupac.get(item)
-        
-        print('{} {}'.format(seq, output_re), file = args.outfile)
-    
-    print('Done, see {}', format(args.outfile))
 
-    
+        print('{} {}'.format(output_seq, output_re), file=args.outfile)
 
+    if args.outfile is not sys.stdout:
+        print('Done, see output in "{}"'.format(args.outfile.name))
 
-    
 
 # --------------------------------------------------
 if __name__ == '__main__':
