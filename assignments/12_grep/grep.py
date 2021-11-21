b24d75ee0932667d91efcf0a09db259c4538b73b
Mery <maria.touceda.suarez@gmail.com>
2021-11-21 15:33:09 -0500

    up to test2

---
+++ assignments/12_grep/grep.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""
+Author : mtoucedasuarez <mtoucedasuarez@email.arizona.edu>
+Date   : 2021-11-21
+Purpose: Python grep
+"""
+
+import argparse
+import sys
+import re
+import os
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Python grep',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('PATTERN',
+                        metavar='str',
+                        help='A positional argument')
+
+    parser.add_argument('FILE',
+                        nargs = '*',
+                        help='Input file(s)',
+                        metavar='FILE',
+                        type=argparse.FileType('rt'))
+    
+    parser.add_argument('-i',
+                        '--insensitive',
+                        help='Case-insensitive search',
+                        action='store_true',
+                        default=False)
+    
+    parser.add_argument('-o',
+                        '--outfile',
+                        help='Output)',
+                        metavar='FILE',
+                        type=argparse.FileType('wt'), 
+                        default=sys.stdout)
+
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+
+    for fh in args.FILE:
+        print('{}:'.format(fh.name) if len(args.FILE) > 1 else '')
+        for line in fh:
+            search = re.search(args.PATTERN, line, re.I) if args.insensitive else re.search(args.PATTERN, line) 
+            if search: 
+                print(line)
+
+    
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
